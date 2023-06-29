6201fbc3c48e0d714ae8c18840e12c04cb9cd3ab
João Vitor Magri <magrijoaovitor@gmail.com>
2023-06-28 23:28:29 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,54 +1,26 @@
-import networkx as nx
-import matplotlib.pyplot as plt
-import scipy.sparse as sp
+import numpy as np
 
-# Create a directed graph to represent transactions
-G = nx.DiGraph()
+adjacency_matrix_normalized = np.array([
+    [0, 0, 1/2, 0, 0],
+    [1/3, 0, 0, 0, 0],
+    [0, 0, 0, 1, 0],
+    [1/3, 0, 0, 0, 1],
+    [1/3, 1/2, 1, 0, 0]
+])
 
-# Add nodes for each account involved in the transactions
-accounts = ["Alice", "Bruno", "Carlos", "Douglas", "Evandro", "Frederico", "Gustavo", "Henrique", "Isabela"]
-G.add_nodes_from(accounts)
+eigenvalues, eigenvectors = np.linalg.eig(adjacency_matrix_normalized)
 
-# Add edges to represent transactions between accounts
-transactions = [("Alice", "Bruno", {"amount": 100}),
-                ("Alice", "Carlos", {"amount": 200}),
-                ("Bruno", "Carlos", {"amount": 50}),
-                ("Carlos", "Douglas", {"amount": 500}),
-                ("Douglas", "Evandro", {"amount": 1000}),
-                ("Frederico", "Alice", {"amount": 300}),
-                ("Evandro", "Frederico", {"amount": 400}),
-                ("Gustavo", "Douglas", {"amount": 200}),
-                ("Carlos", "Gustavo", {"amount": 100}),
-                ("Frederico", "Gustavo", {"amount": 150}),
-                ("Bruno", "Henrique", {"amount": 100}),
-                ("Henrique", "Isabela", {"amount": 100}),
-                ("Isabela", "Bruno", {"amount": 100})]
-G.add_edges_from(transactions)
+positive_eigenvector = None
+for i in range(len(eigenvalues)):
+    autovector = eigenvectors[:, i]
+    if all(coord > 0 for coord in autovector):
+        positive_eigenvector = autovector
+        break
 
-# Calculate the PageRank score for each account
-pr = nx.pagerank(G)
-
-# Find the account with the highest PageRank score
-max_pr_account = max(pr, key=pr.get)
-
-# Convert the PageRank scores to a sparse matrix
-pr_sparse = sp.dok_matrix((1, len(accounts)))
-for i, account in enumerate(accounts):
-    pr_sparse[0, i] = pr[account]
-
-# Visualize the PageRank scores using a bar chart, highlighting the account with the highest score
-color_map = ["red" if account == max_pr_account else "blue" for account in accounts]
-plt.bar(accounts, pr.values(), color=color_map)
-plt.title("PageRank Scores")
-plt.xlabel("Account")
-plt.ylabel("Score")
-plt.show()
-
-# Plot the graph, highlighting the account with the highest score
-pos = nx.spring_layout(G, k=0.5, iterations=50)
-pos_labels = {account: (x, y+0.05) for account, (x, y) in pos.items()}
-node_colors = ["red" if account == max_pr_account else "blue" for account in accounts]
-nx.draw_networkx_nodes(G, pos, node_color=node_colors)
-nx.draw_networkx_edges(G, pos)
-nx.draw_networkx_labels(G, pos_labels, labels={account: account for account in accounts}, font_size=12)
-plt.show()
+if positive_eigenvector is not None:
+    normalized_positive_eigenvector = positive_eigenvector / np.sum(positive_eigenvector)
+    real_normalized_positive_eigenvector = np.real(normalized_positive_eigenvector)
+    print("Autovetor normalizado com soma igual a 1 e coordenadas estritamente positivas:")
+    print(real_normalized_positive_eigenvector)
+else:
+    print("Nenhum autovetor com todas as coordenadas estritamente positivas foi encontrado.")
